b44c0b3753d67f9a023cd11ce5b6d41c559a4562
Thompson Sanjoto <tsanjoto@gmail.com>
2017-04-07 11:06:05 -0700

    disable google recaptcha

--- option.js
+++ option.js
@@ -2,7 +2,7 @@
 var option = {};
 option.apikey = "94dd8825-669d-44f3-ae1a-5f0828016ae6";
 option.appName = "http://lr-candidate-demo3.hub.loginradius.com/";
-option.V2Recaptcha = true;
+option.V2Recaptcha = false;
 option.inFormvalidationMessage = true;
 option.enableLoginOnEmailVerification = true;
 /** You can directly bind it with the correct url, the string operation is just to dynamically bind them**/
