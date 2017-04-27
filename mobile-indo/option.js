1e136c1b9b3c62d45e3e12f450b1c01f1386d270
Thompson Sanjoto <extempest@gmail.com>
2017-04-27 16:36:48 -0700

    test with google disabled

---
+++ mobile-indo/option.js
@@ -0,0 +1,12 @@
+
+var option = {};
+option.apikey = "aad1d378-8613-429b-b728-bb2550e453f3";
+option.appName = "lr-thompson";
+//"http://lr-thompson.hub.loginradius.com/";
+option.V2Recaptcha = false;
+option.inFormvalidationMessage = true;
+option.enableLoginOnEmailVerification = true;
+/** You can directly bind it with the correct url, the string operation is just to dynamically bind them**/
+var path = window.location.href;
+option.emailVerificationUrl = path.replace(path.substr(path.lastIndexOf('/')), "/email-verification.html");
+option.forgotPasswordUrl = path.replace(path.substr(path.lastIndexOf('/')), "/reset-password.html");
