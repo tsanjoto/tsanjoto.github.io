93f9bfc8e7405b73076623b63fe164a284ea7cd0
Thompson Sanjoto <thompson.sanjoto@loginradius.com>
2017-05-10 16:45:12 -0700

    Add observer

--- mobile/js/dynamic-action.js
+++ mobile/js/dynamic-action.js
@@ -4,8 +4,52 @@ var message = {
 
 };
 
-Object.observe(window.location.href, function(changes) {
-  console.log(changes);
+if (!Object.prototype.watch) {
+	Object.defineProperty(Object.prototype, "watch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop, handler) {
+			var
+			  oldval = this[prop]
+			, newval = oldval
+			, getter = function () {
+				return newval;
+			}
+			, setter = function (val) {
+				oldval = newval;
+				return newval = handler.call(this, prop, oldval, val);
+			}
+			;
+			
+			if (delete this[prop]) { // can't watch constants
+				Object.defineProperty(this, prop, {
+					  get: getter
+					, set: setter
+					, enumerable: true
+					, configurable: true
+				});
+			}
+		}
+	});
+}
+
+// object.unwatch
+if (!Object.prototype.unwatch) {
+	Object.defineProperty(Object.prototype, "unwatch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop) {
+			var val = this[prop];
+			delete this[prop]; // remove accessors
+			this[prop] = val;
+		}
+	});
+}
+
+window.location.watch("href", function(id, old, nu) {
+	console.log( id +' '+ old +' '+ nu);
 });
 
 function setCookie(cname, cvalue, exdays) {
